079b7e422bafe8ecfe1ae53323383353a5ca94df
Vitor M. Basso <vitorbasso@users.noreply.github.com>
2017-09-14 18:59:05 -0300

    TIpo
    Mudança no tipo das variáveis declaradas para o tipo pai.

--- poo2lab1/src/poo2lab1/Poo2lab1.java
+++ poo2lab1/src/poo2lab1/Poo2lab1.java
@@ -17,13 +17,13 @@ public class Poo2lab1 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        Persona1 principal = new Persona1("Mago", 0, 0);
-        Persona2 principal2 = new Persona2("Cavaleiro", 0, 0);
-        Persona3 principal3 = new Persona3("Arqueiro", 0, 0);
+        Personagem principal = new Persona1("Mago", 0, 0);
+        Personagem principal2 = new Persona2("Cavaleiro", 0, 0);
+        Personagem principal3 = new Persona3("Arqueiro", 0, 0);
         
-        Enemy1 enemy1 = new Enemy1("Arqueiro maligno");
-        Enemy2 enemy2 = new Enemy2("Mago maligno");
-        Enemy3 enemy3 = new Enemy3("Cavaleiro maligno");
+        ObservingEnemy enemy1 = new Enemy1("Arqueiro maligno");
+        ObservingEnemy enemy2 = new Enemy2("Mago maligno");
+        ObservingEnemy enemy3 = new Enemy3("Cavaleiro maligno");
         
         System.out.println("Personagem 1: \n");
         principal.getAtac();
